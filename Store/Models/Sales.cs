3e0b1e0700b504b8d629da2a7ec44df062f04fb5
bablu.net101@gmail.com <info@iizii.in>
2022-11-08 20:22:37 +0530

    new changes

---
+++ Store/Models/Sales.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Sales
+    {
+       
+        public DateTime fromDate { get; set; }
+        public DateTime endDate { get; set; }
+    }
+
+    public class FillterBrand
+    {
+        public string brand { get; set; }
+        public DateTime fromDate { get; set; }
+        public DateTime endDate { get; set; }
+    }
+
+    public class ProfitLoss
+    {
+        public string Name { get; set; }
+        public string Mbrand { get; set; }
+        public decimal Loss { get; set; }
+        public decimal Profit { get; set; }
+        public decimal Discount { get; set; }
+        
+    }
+}
---
+++ Store/Models/Store.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Store
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mbrand { get; set; }
+        public decimal SalePrice { get; set; }
+        public decimal PurchPrice { get; set; }
+        public int pro_loss { get; set; }
+        public decimal Discount { get; set; }
+        public DateTime Dates { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
